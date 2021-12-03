34552430a014323e3414e89b78e370b8704d863c
Gustav Sandström <gustav.sandstrom@nentgroup.com>
2021-12-03 09:24:07 +0100

    cleanup

--- day3/part2.js
+++ day3/part2.js
@@ -8,7 +8,7 @@ const count = ( acc, el ) => {
     return acc;
 };
 
-const file = fs.readFileSync('./input.txt');
+const file = fs.readFileSync('./sample_input.txt');
 let lines = file.toString().split("\n");
 lines = lines.map(s => s.trim());
 const elemLength = lines[0].length;
@@ -19,50 +19,43 @@ function colCountFun(lines){
         const col = lines.map(s => s.charAt(i));
         cols.push(col)
     }
-
-    const colCount = cols.map(arr => arr.reduce(count, {}));
-    return colCount;
+    return cols.map(arr => arr.reduce(count, {}));
 }
 
-let candidates = lines.slice();
-let colCount= colCountFun(candidates);
-// oxygen generator rating
-for (col in colCount){
-    colCount= colCountFun(candidates);
-    //console.log([colCount[col]['1'],colCount[col]['0']])
-    if (colCount[col]['1'] >= colCount[col]['0']){
-        //console.log("looking for 1")
-        candidates = candidates.filter(s => s.charAt(col) === "1")
-    } else {
-        //console.log("looking for 0")
-        candidates = candidates.filter(s => s.charAt(col) === "0")
+function oxygen_function(lines) {
+    let candidates = lines.slice();
+    let colCount = colCountFun(candidates);
+    for (let col in colCount) {
+        colCount = colCountFun(candidates);
+        if (colCount[col]['1'] >= colCount[col]['0']) {
+            candidates = candidates.filter(s => s.charAt(col) === "1")
+        } else {
+            candidates = candidates.filter(s => s.charAt(col) === "0")
+        }
+        if (candidates.length < 2) {
+            break;
+        }
     }
-    //console.log(candidates);
-    if (candidates.length < 2){
-        break;
-    }
+    return parseInt(candidates[0], 2);
 }
-const oxygen_int = parseInt(candidates[0], 2);
 
-candidates = lines.slice();
-colCount= colCountFun(candidates);
-// co2 scrubb rating
-for (col in colCount){
-    colCount= colCountFun(candidates);
-    console.log([colCount[col]['1'],colCount[col]['0']])
-    if (colCount[col]['0'] <= colCount[col]['1']){
-        console.log("looking for 0")
-        candidates = candidates.filter(s => s.charAt(col) === "0")
-    } else {
-        console.log("looking for 1")
-        candidates = candidates.filter(s => s.charAt(col) === "1")
+function co2_scrub_function(lines) {
+    let candidates = lines.slice();
+    let colCount = colCountFun(candidates);
+    for (let col in colCount) {
+        colCount = colCountFun(candidates);
+        if (colCount[col]['0'] <= colCount[col]['1']) {
+            candidates = candidates.filter(s => s.charAt(col) === "0")
+        } else {
+            candidates = candidates.filter(s => s.charAt(col) === "1")
+        }
+        if (candidates.length < 2) {
+            break;
+        }
     }
-    console.log(candidates);
-    if (candidates.length < 2){
-        break;
-    }
+    return parseInt(candidates[0], 2);
 }
-const co2_scrub = parseInt(candidates[0], 2);
 
-console.log([oxygen_int, co2_scrub])
+const co2_scrub = co2_scrub_function(lines);
+const oxygen_int = oxygen_function(lines);
 console.log(oxygen_int*co2_scrub)
