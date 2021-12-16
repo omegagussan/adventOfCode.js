4c43372a9a86840070f41287f7a6831b82e3e5de
Gustav Sandström <gustav.sandstrom@nentgroup.com>
2021-12-16 23:19:34 +0100

    functions

--- src/day16/part2.js
+++ src/day16/part2.js
@@ -7,52 +7,64 @@ let binSeq = intSeq.map(i => [...i.toString(2).padStart(4, '0')]).flat(1)
 
 const LITERAL_STRIDE = 5;
 const TO_BINARY = 2;
+const BIT_LENGTH_STRIDE = 15;
+const SUB_PACKET_STRIDE = 11;
+
+function stepAndUpdate(content, subP) {
+    const p = parsePacket(content);
+    content = p.rest;
+    subP.push(p.packet);
+    return content;
+}
+
+function subPacketFunction(chars, version, typeId) {
+    const numSubPackets = Number.parseInt(chars.splice(0, SUB_PACKET_STRIDE).join(''), TO_BINARY)
+    let content = chars.join('');
+
+    let subP = []
+    for (let i = 0; i < numSubPackets; i++) {
+        content = stepAndUpdate(content, subP);
+
+    }
+    const packet = {'literal': false, version, typeId, subPackets: subP}
+    return {packet, rest: content}
+}
+
+function bitLengthFunction(chars, version, typeId) {
+    const bitLength = Number.parseInt(chars.splice(0, BIT_LENGTH_STRIDE).join(''), TO_BINARY)
+    let content = chars.splice(0, bitLength).join('');
+
+    let subP = []
+    while (content.length > 0) {
+        content = stepAndUpdate(content, subP);
+    }
+    const packet = {'literal': false, version, typeId, subPackets: subP}
+    return {packet, rest: chars.join('')}
+}
+
+function LiteralFunction(chars, version, typeId) {
+    let literalBinary = '';
+    while (chars.length > 0) {
+        const sub = chars.splice(0, LITERAL_STRIDE).join('').padEnd(LITERAL_STRIDE, '0');
+        literalBinary += sub.slice(1);
+        if (sub[0] === '0') break;
+    }
+    const packet = {'literal': true, version, typeId, value: Number.parseInt(literalBinary, TO_BINARY)}
+    return {packet, rest: chars.join('')}
+}
+
 function parsePacket(bin){
     const chars = bin.split('');
     let version = Number.parseInt(chars.splice(0,3).join(''), TO_BINARY);
     let typeId = Number.parseInt(chars.splice(0, 3).join(''), TO_BINARY);
 
-    if (typeId === 4) {
-        let literalBinary = '';
+    if (typeId === 4) return LiteralFunction(chars, version, typeId)
 
-        while (chars.length > 0) {
-            const sub = chars.splice(0, LITERAL_STRIDE).join('').padEnd(LITERAL_STRIDE, '0');
-            literalBinary += sub.slice(1);
-            if (sub[0] === '0') break;
-        }
-        const packet = {'literal': true, version, typeId, value: Number.parseInt(literalBinary, TO_BINARY)}
-        return {packet, rest: chars.join('')}
-    }
-
-    const BIT_LENGTH_STRIDE = 15;
-    const SUB_PACKET_STRIDE = 11;
     const lengthTypeId = chars.shift();
     if (lengthTypeId === '0'){
-        const bitLength = Number.parseInt(chars.splice(0, BIT_LENGTH_STRIDE).join(''), TO_BINARY)
-        let content = chars.splice(0, bitLength).join('');
-
-        let subP = []
-        while(content.length > 0){
-            const p = parsePacket(content);
-            content = p.rest;
-            subP.push(p.packet);
-        }
-        const packet = {'literal': false, version, typeId, subPackets: subP}
-        return {packet, rest: chars.join('')}
-
+        return bitLengthFunction(chars, version, typeId);
     } else {
-        const numSubPackets = Number.parseInt(chars.splice(0, SUB_PACKET_STRIDE).join(''), TO_BINARY)
-
-        let content = chars.join('');
-
-        let subP = []
-        for(let i=0; i< numSubPackets; i++){
-            const p = parsePacket(content);
-            content = p.rest;
-            subP.push(p.packet);
-        }
-        const packet = {'literal': false, version, typeId, subPackets: subP}
-        return {packet, rest: content}
+        return subPacketFunction(chars, version, typeId);
     }
 
 }
@@ -60,22 +72,22 @@ function parsePacket(bin){
 function reduce(packet) {
     if (packet.literal) return packet.value;
 
-    const subPacketValues = packet.subPackets.map(subPacket => reduce(subPacket));
+    const vals = packet.subPackets.map(subPacket => reduce(subPacket));
     switch (packet.typeId) {
         case 0:
-            return subPacketValues.reduce((a, b) => a + b);
+            return vals.reduce((a, b) => a + b);
         case 1:
-            return subPacketValues.reduce((a, b) => a * b);
+            return vals.reduce((a, b) => a * b);
         case 2:
-            return Math.min(...subPacketValues);
+            return Math.min(...vals);
         case 3:
-            return Math.max(...subPacketValues);
+            return Math.max(...vals);
         case 5:
-            return subPacketValues[0] > subPacketValues[1] ? 1 : 0;
+            return vals[0] > vals[1] ? 1 : 0;
         case 6:
-            return subPacketValues[0] < subPacketValues[1] ? 1 : 0;
+            return vals[0] < vals[1] ? 1 : 0;
         case 7:
-            return subPacketValues[0] === subPacketValues[1] ? 1 : 0;
+            return vals[0] === vals[1] ? 1 : 0;
         default:
             throw new Error(packet.typeId);
     }
