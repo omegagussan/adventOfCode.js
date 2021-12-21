4229d359733591dfcd8e6a469a86551e7befea11
Gustav Sandström <gustav.sandstrom@nentgroup.com>
2021-12-21 11:27:20 +0100

    temp

--- src/day21/part2.js
+++ src/day21/part2.js
@@ -1,49 +1,47 @@
 const fs = require('fs');
 
-function inclusiveRange(from, to) {
-    return [...Array(to +1).keys()].slice(from)
-}
-
-function* infiniteGen(arr){
-    if(!Array.isArray(arr)){throw Error("not array")}
-    while(true){
-        yield* arr
-    }
-}
-let testDie = infiniteGen(inclusiveRange(1, 4))
-let test = testDie.next().value + testDie.next().value + testDie.next().value
-let test2 = testDie.next().value + testDie.next().value + testDie.next().value
-console.assert(test === 6, 'first 3 rolls did not equal 6')
-console.assert(test2 === 7, 'next 3 rolls did not equal 7')
-
-const die = infiniteGen(inclusiveRange(1, 100))
-
-const file = fs.readFileSync('./input.txt');
+const file = fs.readFileSync('./sample_input.txt');
 let state = file.toString().split('\n').map(l => l.slice(-1)[0]).map(e => +e).map(e => e-1)
 let score = [0, 0]
 console.log(state)
 
-function play(player, die, state, score) {
-    let rollDie = 0
-    for (let i=0; i<3; i++){
-        let next = die.next().value
-        rollDie += next
+function play(player, state, score) {
+    let states = [];
+    for(let j=0; j < 3; j++){
+        let quantumDie = [1,2,3]
+        quantumDie.forEach(roll => {
+            let stateFork = [...state]
+            let scoreFork = [...score]
+            stateFork[player] = (stateFork[player] + roll) % 10
+            scoreFork[player] = scoreFork[player] + stateFork[player] + 1
+            states.push({state: stateFork, score: scoreFork})
+        })
     }
-    state[player] = (state[player] + rollDie) % 10
-    score[player] = score[player] + state[player] + 1
+    return states;
 }
 
 let player = 0
-while(true){
-    play(player % 2, die, state, score)
+let ongoing = [{state, score}]
+let wins = [0, 0]
+while(ongoing.length > 0){
+    let before = ongoing.length
+    for (let i=0; i < before; i++){
+        let {state:st, score:sc} = ongoing.shift()
+        let outcomes = play(player % 2, st, sc)
+        ongoing.push(...outcomes)
+    }
+    ongoing = ongoing.filter(game => {
+        if (game.score[0] >= 21){
+            wins[0] += 1
+            return false
+        }
+        if (game.score[1] >= 21){
+            wins[1] += 1
+            return false
+        }
+        return true
+    })
     player += 1
-    //console.log('x', score)
-    //console.log('s', state)
-    //console.log(' ')
-    if (score.some(e => e >= 1000)) break
 }
-let looser = Math.min(...score)
-let rolls = player *3
-console.log(looser)
-console.log(rolls)
-console.log(rolls*looser)
+console.log(wins)
+console.log(Math.max(...wins))
